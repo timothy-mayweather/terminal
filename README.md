817d0598af695911a328c134703dcb8817541a5d
timothy kibalama <timkibalama@gmail.com>
2023-03-21 02:17:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+terminal interaction in php
---
+++ terminal.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if(!isset($_SESSION["directory"])){
+    $directory = getcwd();
+    $user = explode('/', $directory)[2];
+    $_SESSION["user"] = $user;
+    $_SESSION["directory"] = '/home/'.$user;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $command = $_POST['command'];
+
+    if (!empty($command)) {
+        $output = shell_exec('cd '.$_SESSION["directory"].' && '.$command.' && echo ---$PWD');
+        $str = strstr($output, '---/');
+        $output = str_replace($str, '', $output);
+        $_SESSION["directory"] = str_replace('---', '', trim($str));
+        echo json_encode(['output' => $output, 'directory' => str_replace('/home/'.$_SESSION["user"], '~', $_SESSION["directory"]), 'user' => $_SESSION["user"]]);
+    }
+}
+else{
+    echo json_encode(['directory' => str_replace('/home/'.$_SESSION["user"], '~', $_SESSION["directory"]), 'user' => $_SESSION["user"]]);
+}
