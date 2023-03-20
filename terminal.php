770015cb7ecc0715a211c6175d8420f32ad8c2ed
timothy kibalama <timkibalama@gmail.com>
2023-03-21 02:51:21 +0300

    removed invalid output

--- terminal.php
+++ terminal.php
@@ -12,10 +12,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!empty($command)) {
         $output = shell_exec('cd '.$_SESSION["directory"].' && '.$command.' && echo ---$PWD');
-        $str = strstr($output, '---/');
-        $output = str_replace($str, '', $output);
-        $_SESSION["directory"] = str_replace('---', '', trim($str));
-        echo json_encode(['output' => $output, 'directory' => str_replace('/home/'.$_SESSION["user"], '~', $_SESSION["directory"]), 'user' => $_SESSION["user"]]);
+        if(strlen(trim($output))>0){
+            $str = strstr($output, '---/');
+            $output = str_replace($str, '', $output);
+            $_SESSION["directory"] = str_replace('---', '', trim($str));
+        }
+        echo json_encode(['output' => $output??'', 'directory' => str_replace('/home/'.$_SESSION["user"], '~', $_SESSION["directory"]), 'user' => $_SESSION["user"]]);
     }
 }
 else{
